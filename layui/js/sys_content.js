3375c52b3840b86f91a07e2f5625f31e205d473b
vip <1529796910@qq.com>
2018-01-03 15:35:13 +0800

    家里修改

--- layui/js/sys_content.js
+++ layui/js/sys_content.js
@@ -19,4 +19,59 @@ $(function(){
 	        $('.obj-text  .text-box').eq(i).siblings().hide();
 	    })
 	});
+	// 点击变换不同背景颜色
+	$('.obj-title .li1').click(function(){
+		$(this).addClass('li1-click');
+		$(this).children('i').addClass('li1-i');
+
+		$('.obj-title .li2').removeClass('li2-click');
+		$('.obj-title .li2').children('i').removeClass('li2-i');
+
+		$('.obj-title .li3').removeClass('li3-click');
+		$('.obj-title .li3').children('i').removeClass('li3-i');
+
+		$('.obj-title .li4').removeClass('li4-click');
+		$('.obj-title .li4').children('i').removeClass('li4-i');
+	})
+	$('.obj-title .li2').click(function(){
+		$(this).addClass('li2-click');
+		$(this).children('i').addClass('li2-i');
+
+		$('.obj-title .li1').removeClass('li1-click');
+		$('.obj-title .li1').children('i').removeClass('li1-i');
+
+		$('.obj-title .li3').removeClass('li3-click');
+		$('.obj-title .li3').children('i').removeClass('li3-i');
+
+		$('.obj-title .li4').removeClass('li4-click');
+		$('.obj-title .li4').children('i').removeClass('li4-i');
+	})
+
+	$('.obj-title .li3').click(function(){
+		$(this).addClass('li3-click');
+		$(this).children('i').addClass('li3-i');
+
+		$('.obj-title .li1').removeClass('li1-click');
+		$('.obj-title .li1').children('i').removeClass('li1-i');
+
+		$('.obj-title .li2').removeClass('li2-click');
+		$('.obj-title .li2').children('i').removeClass('li2-i');
+
+		$('.obj-title .li4').removeClass('li4-click');
+		$('.obj-title .li4').children('i').removeClass('li4-i');
+	})
+
+	$('.obj-title .li4').click(function(){
+		$(this).addClass('li4-click');
+		$(this).children('i').addClass('li4-i');
+
+		$('.obj-title .li1').removeClass('li1-click');
+		$('.obj-title .li1').children('i').removeClass('li1-i');
+
+		$('.obj-title .li2').removeClass('li2-click');
+		$('.obj-title .li2').children('i').removeClass('li2-i');
+
+		$('.obj-title .li3').removeClass('li3-click');
+		$('.obj-title .li3').children('i').removeClass('li3-i');
+	})
 })
