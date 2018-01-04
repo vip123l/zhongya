9c0e3a5b50433642aad40c7b4911dbbbbb0b72d1
vip <vip@PC201711231640>
2018-01-04 16:25:02 +0800

    添加弹框

--- layui/js/sys_content.js
+++ layui/js/sys_content.js
@@ -19,6 +19,55 @@ $(function(){
 	        $('.obj-text  .text-box').eq(i).siblings().hide();
 	    })
 	});
+
+	//  添加专业弹框
+layui.use('form', function(){
+	var form = layui.form;
+	$('.zy,.table1 .editor').on('click',function(){
+		layer.open({
+			type: 1,
+			title:'添加专业',
+			btn: ['确定'],
+			skin: 'add-layer', 
+			area: ['720px', '360px'], //宽高
+			content: $('.add-zy').html(),
+			success:function(){
+				form.render('select')
+			}
+		});
+	});
+});	
+
+	//  添加报名考件弹框
+layui.use('form', function(){
+	var form = layui.form;
+	$('.alert-bm,.table2 .editor').on('click',function(){
+		layer.open({
+			type: 1,
+			title:'添加报名考件',
+			btn: ['确定'],
+			skin: 'add-layer', 
+			area: ['720px', '660px'], //宽高
+			content: $('.add-bk').html(),
+			success:function(){
+				form.render('select')
+			}
+		});
+	});
+});
+
+layui.use('layer', function(){
+    var layer = layui.layer;
+    //	删除弹框
+	$('.delete').on('click',function(){
+		layer.confirm('确定要删除此条内容吗？', {
+			title: '删除',
+			btn: ['确定', '取消'],
+			skin: 'layer-del',
+		});
+	})
+}); 
+
 	// 点击变换不同背景颜色
 	$('.obj-title .li1').click(function(){
 		$(this).addClass('li1-click');
