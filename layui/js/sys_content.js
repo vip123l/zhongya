78fdd1536e5b775295649e594109fc6071bc7975
haozhenhua <haozhenhua@neoteched.com>
2018-01-09 12:41:49 +0800

    调整

--- layui/js/sys_content.js
+++ layui/js/sys_content.js
@@ -46,45 +46,54 @@ layui.use(['form','layedit','layer'] ,function(){
 			area: ['910px', '660px'], //宽高
 			content: $('.add-bk'),
 		});
-//		//添加专业富文本编辑器
-//		  layedit.build('font-set2', {
-//		    tool: ['strong','italic', '|', 'left', 'center', 'right']
-//		    ,height: 400
-//		  }); //建立编辑器
 	});
 	//	删除弹框	
 	$('.delete').on('click',function(){
 		layer.confirm('确定要删除此条内容吗？', {
 			title: '删除',
 			btn: ['确定', '取消'],
+			area: ['430px'],
 			skin: 'layer-del',
 		});
 	})
 	
-		//添加专业内复选框
-		$('.add-check .check-title').click(function () {
-		    if ($('.check-box').css('display') == "none") {
-		        $('.check-box').css('display', 'block');
-		    }
-		    else {
-		        $('.check-box').css('display', 'none');
-		    }
+	//添加专业内复选框
+	$('.add-check .check-title').click(function () {
+	    if ($('.check-box').css('display') == "none") {
+	        $('.check-box').css('display', 'block');
+	        $('.add-check .layui-edge').addClass('layui-edge-checked')
+	    }
+	    else {
+	        $('.check-box').css('display', 'none');
+	        $('.add-check .layui-edge').removeClass('layui-edge-checked')
+	    }
+	});
+//		点击别处收起
+	$('body').click(function(){
+		$('.check-box').css('display', 'none');
+		$('.add-check .layui-edge').removeClass('layui-edge-checked')
+	});
+	$('.add-check').click(function(){
+		event.stopPropagation();
+	})
+
+    form.on('checkbox(test)', function(data){
+        get_box_val();
+    });
+
+});
+//复选框被选中的值
+	function get_box_val() {
+		var val = '';
+		$('.add-check input[type=checkbox]').each(function () {
+			if($(this).prop('checked')){
+				val+=$(this).val()+' ';
+			}
+			$('.check-title .layui-input').val(val);
 		});
-		$('.check-box dd').each(function () {
-			var aa = '';
-				form.on('checkbox(test)', function(data){
-					aa+=data.value;
-				    console.log(data.elem.checked); //是否被选中，true或者false
-				    console.log(data.value); //复选框value值，也可以通过data.elem.value得到
-				    form.render('checkbox');
-				}); 
-				$('.check-title .layui-input').val('aa')
-		    })
-	
-
-});	
-
-
+	}
+	//页面加载完的时候先运行一下
+	get_box_val();
 
 	// 点击变换不同背景颜色
 	$('.obj-title .li1').click(function(){
